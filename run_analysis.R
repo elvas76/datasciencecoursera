443cf7e7dc11122a4d52f5276ba111bf43cd0de0
Andre Elvas <andre.elvas@gmail.com>
2016-08-25 19:33:33 -0300

    fixing errors

--- run_analysis.R
+++ run_analysis.R
@@ -11,14 +11,14 @@ y_test <- read.table("y_test.txt")
 y_train <- read.table("y_train.txt")
 
 #Merges the training and the test sets to create one data set.
-data_test <- cbind(y_test,X_test)
-data_train <- cbind(y_train,X_train)
-data <- rbind(data_test,data_train)
-names(data) <- c("activity_id", as.character(features$V2))
+data_test <- cbind(subject_test, y_test, X_test)
+data_train <- cbind(subject_train, y_train, X_train)
+data <- rbind(data_test, data_train)
+names(data) <- c("subject_id", "activity_id", as.character(features$V2))
 
 #Extracts only the measurements on the mean and standard deviation 
 # for each measurement.
-columns <- sort(c(1,union(grep("mean",names(data)), grep("std",names(data)))))
+columns <- sort(c(1,2, union(grep("mean",names(data)), grep("std",names(data)))))
 data <- data[, columns]
 
 #Uses descriptive activity names to name the activities in the data set
@@ -30,5 +30,5 @@ data <- data[,2:(length(names(data))-1)]
 
 #From the data set in step 4, creates a second, independent tidy data set with 
 # the average of each variable for each activity and each subject.
-data_gpr <- summarise_each(group_by(data, activity_name), funs(mean))
+data_gpr <- summarise_each(group_by(data, activity_name, subject_id), funs(mean))
 write.table(data_gpr,file="tidy_dataset.txt",row.name=FALSE)
